1851b4122f378aa7b14ffb5140b585322a89ba0a
gaeun <gaeun@gan-ui-noteubug.local>
2024-02-20 00:56:41 +0900

    [FEAT] DB connection pool

--- src/main/java/store/ckin/coupon/CouponApplication.java
+++ src/main/java/store/ckin/coupon/CouponApplication.java
@@ -2,8 +2,12 @@ package store.ckin.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CouponApplication {
 
 	public static void main(String[] args) {
